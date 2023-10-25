1d65306185a545481994a841b32aecf569b91fbb
Mirantsoa <mirantsoarabeantoandro@gmail.com>
2023-10-25 23:17:42 +0300

    Creating order list of all tables

---
+++ src/screens/chef/CommandChef/Tables/ListTable.js
@@ -0,0 +1,84 @@
+const ListTable = [
+    {
+        title: 'Table 3',
+        data:[
+        {
+            id: 1,
+            title:'Mine-Sao Special',
+            quantity: 1,
+        },
+        {
+            id: 2,
+            title:'Pizza Royal',
+            quantity: 2,
+        },
+        {
+            id: 3,
+            title:'Gratin de pates',
+            quantity: 1,
+        },
+        ],
+    },
+    {
+        title: 'Table 5',
+        data:[
+            {
+                id: 4,
+                title:'Glace au choco',
+                quantity: 1,      
+            },
+            {
+                id: 2,
+                title:'Pizza Royale',
+                quantity: 2,
+            },
+            {
+                id: 6,
+                title:'Gratin de crevettes',
+                quantity: 1,
+            },
+        ],  
+    },
+    {
+        title: 'Table 1',
+        data:[
+            {
+                id: 7,
+                title:'Poulet frit',
+                quantity: 1,      
+            },
+            {
+                id: 8,
+                title:'Soupe Tamatave',
+                quantity: 2,
+            },
+            {
+                id: 3,
+                title:'Gratin de pâtes',
+                quantity: 1,
+            },
+        ],  
+    },
+    {
+        title: 'Table 7',
+        data:[
+            {
+                id: 10,
+                title:'Brochettes',
+                quantity: 10,      
+            },
+            {
+                id: 11,
+                title:'Frites + Steack',
+                quantity: 2,
+            },
+            {
+                id: 1,
+                title:'Mine-Sao Spéciale',
+                quantity: 1,
+            },
+        ],  
+    },
+]
+
+export default ListTable;
