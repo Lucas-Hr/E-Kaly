514ed26c29037d5a4bb230077c36fe94a35e4c94
Mirantsoa <mirantsoarabeantoandro@gmail.com>
2023-10-25 17:18:26 +0300

    Updating Liste to ListClient

---
+++ src/screens/client/CommandCli/ListClient/ListClient.js
@@ -0,0 +1,50 @@
+const ListClient = [
+    {
+        title: 'Nos Commandes',
+        data:[
+        {
+            id: 1,
+            title:'Mine-Sao Special',
+            price: 15.200,
+            quantity: 2,
+        },
+        {
+            id: 2,
+            title:'Pizza Royal',
+            price: 22.000,
+            quantity: 3,
+        },
+        {
+            id: 3,
+            title:'Gratin de pates',
+            price: 20.000,
+            quantity: 1,
+        },
+        ],
+    },
+    {
+        title: 'Mes Commandes',
+        data:[
+            {
+                id: 4,
+                title:'Ravitoto',
+                price: 10.000,
+                quantity: 1,      
+            },
+            {
+                id: 5,
+                title:'Gratin de pates',
+                price: 20.000,
+                quantity: 1,
+            },
+            {
+                id: 6,
+                title:'Choco crème',
+                price: 4.000,
+                quantity: 1,
+            },
+        ],  
+    }
+]
+
+export default ListClient;
